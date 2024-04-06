7b534aedb793f1c762db97bc6c0940da73a9d889
Devon Sawatsky <novedevo@gmail.com>
2024-04-05 17:45:09 -0700

    "improve" cli

--- src/bin.rs
+++ src/bin.rs
@@ -8,19 +8,26 @@ use imoji::image_to_emoji;
 #[command(version, about, long_about = None)]
 struct Cli {
     /// Image file to be emojified
-    #[arg(short, long, value_name = "FILE")]
-    input: PathBuf,
+    input_file: PathBuf,
 
     /// Restrict the output emoji grid to a certain width
+    #[arg(long, name = "width")]
     max_width: Option<u32>,
-    /// Restrict the output emoji grid to a certain width
+    /// Restrict the output emoji grid to a certain height
+    #[arg(long, name = "height")]
     max_height: Option<u32>,
+    /// output for monospace (default true)
+    #[arg(long, short)]
+    monospace: Option<bool>,
 }
 
 fn main() -> Result<(), ImageError> {
     let cli = Cli::parse();
-    let img = ImageReader::open(cli.input)?.decode()?;
-    let emoji_grid = image_to_emoji(img, cli.max_width, cli.max_height);
+    let img = ImageReader::open(cli.input_file)?.decode()?;
+    let mut emoji_grid = image_to_emoji(img, cli.max_width, cli.max_height);
+    if let Some(true) | None = cli.monospace {
+        emoji_grid = emoji_grid.replace('▪', "  ")
+    }
     println!("{emoji_grid}");
     Ok(())
 }
